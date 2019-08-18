fb974e6f1118f6a0ebdc39b0351ff577303649d9
Yasin Odabas <ysnodabas@gmail.com>
2019-08-18 14:03:19 +0800

    first upload

---
+++ src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    //properties for state object
+    count: 0
+  };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  };
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
