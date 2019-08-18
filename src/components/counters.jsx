7579214fac4211d91e97753f02c818db20005e8c
Yasin Odabas <ysnodabas@gmail.com>
2019-08-18 21:10:00 +0800

    id6 added

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -8,7 +8,8 @@ class Counters extends Component {
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
-      { id: 5, value: 0 }
+      { id: 5, value: 0 },
+      { id: 6, value: 0 }
     ]
   };
 
