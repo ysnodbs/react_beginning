06340fc5c9c4fd389f817eb0fd26084377566d30
kwan1 <21560466@student.uwa.edu.au>
2019-08-18 20:46:05 +0800

    delete button added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     //properties for state object
-    value: this.props.counter.value
+   value: this.props.counter.value
+
   };
 
   // constructor() {
@@ -11,25 +12,25 @@ class Counter extends Component {
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
   handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
+    this.setState({value: this.state.value + 1});
   };
 
   render() {
+
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={ () => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
+        onClick={() => this.props.onDelete(this.props.counter.id)}
+        className="btn btn-danger btn-sm m-2" 
         >
-          {" "}
-          Delete
+        Delete
         </button>
       </div>
     );
@@ -40,7 +41,7 @@ class Counter extends Component {
     return classes;
   }
   formatCount() {
-    const { value } = this.state;
+    const { value} = this.state;
     return value === 0 ? "Zero" : value;
   }
 }
